1e4b97a5ec989f1253e5989e3c09254b22468b88
mujdecisy <mujdecisy@gmail.com>
2023-12-09 01:01:22 +0300

    feat: update chart and actions

--- app.py
+++ app.py
@@ -1,83 +1,108 @@
 import json
 from flask import Flask, render_template, request
 from flask_basicauth import BasicAuth
-import yfinance as yf
 import os
+import diti
+from datetime import datetime
+import urllib
+
+from helper import get_initial_filters, prepare_index_page_data
 
 app = Flask(__name__)
 MODE = os.environ.get("APP_MODE", "debug")
 
 print(f"MODE = {MODE}")
 
-app.config['BASIC_AUTH_USERNAME'] = os.environ.get("APP_BA_USERNAME", "")
-app.config['BASIC_AUTH_PASSWORD'] = os.environ.get("APP_BA_PASSWORD", "")
+app.config["BASIC_AUTH_USERNAME"] = os.environ.get("APP_BA_USERNAME", "")
+app.config["BASIC_AUTH_PASSWORD"] = os.environ.get("APP_BA_PASSWORD", "")
 
 basic_auth = BasicAuth(app)
+
 
 @app.route("/")
 @basic_auth.required
 def index():
-    data_type = request.args.get("dataType", "Close")
-    data_period = request.args.get("dataPeriod", "1mo")
-    data_interval = request.args.get("dataInterval", "1d")
+    init_from, init_to = get_initial_filters()
 
-    data = yf.download(
-        "TRY=X XU100.IS GC=F BTC-USD", period=data_period, interval=data_interval
-    )
-    data = data[
-        [
-            (data_type, "TRY=X"),
-            (data_type, "XU100.IS"),
-            (data_type, "GC=F"),
-            (data_type, "BTC-USD"),
-        ]
-    ]
-    if data_interval == "1h":
-        data.index = data.index.floor("H")
-        data = data.groupby(data.index).mean()
-    elif data_interval == "15m":
-        data.index = data.index.floor("15T")
-        data = data.groupby(data.index).mean()
-    data = data.dropna()
-    data[(data_type, "GC=FxTRY=X")] = (
-        data[(data_type, "GC=F")] * data[(data_type, "TRY=X")]
-    )
-    data[(data_type, "BTC-USDxTRY=X")] = (
-        data[(data_type, "BTC-USD")] * data[(data_type, "TRY=X")]
+    data_type = "Close"
+    from_diti = request.args.get("fDiti", init_from)
+    to_diti = request.args.get("tDiti", init_to)
+
+    diff = diti.diti_calcs.diff(
+        datetime.fromisoformat(from_diti),
+        datetime.fromisoformat(to_diti),
+        diti.DitiParts.HOURS,
     )
 
-    data = data[
-        [
-            (data_type, "TRY=X"),
-            (data_type, "XU100.IS"),
-            (data_type, "GC=FxTRY=X"),
-            (data_type, "BTC-USDxTRY=X"),
-        ]
-    ]
-    data = data / data.iloc[0]
+    if diff > 8 * 24:
+        data_interval = "1d"
+    elif diff > 2 * 24:
+        data_interval = "1h"
+    else:
+        data_interval = "15T"
 
-    index_mapping = {
-        "TRY=X": {"name": "USD", "color": "red"},
-        "XU100.IS": {"name": "BIST", "color": "blue"},
-        "GC=FxTRY=X": {"name": "GOLD", "color": "green"},
-        "BTC-USDxTRY=X": {"name": "BTC", "color": "purple"},
-    }
+    diti_list, data_dict = prepare_index_page_data(
+        from_diti, to_diti, data_interval, data_type
+    )
+    data_list = [{"name": k, "data": v} for k, v in data_dict.items()]
 
-    dates = [e.strftime("%m-%d %H:%M") for e in data.index.to_list()]
-    data = data.to_dict(orient="list")
-    json_data = []
-    for k, v in data.items():
-        json_data.append(
-            {
-                "label": index_mapping[k[1]]["name"],
-                "data": v,
-                "borderColor": index_mapping[k[1]]["color"],
-            }
-        )
+    from_dt = datetime.fromisoformat(from_diti)
+    to_dt = datetime.fromisoformat(to_diti)
+
+    step_size = int(diff / 4)
+
+    prev_from_diti = urllib.parse.quote_plus(
+        diti.diti_op(
+            from_dt, [diti.DitiOpAdd(diti.DitiParts.HOURS, -step_size)]
+        ).isoformat()
+    )
+    prev_to_diti = urllib.parse.quote_plus(
+        diti.diti_op(
+            to_dt, [diti.DitiOpAdd(diti.DitiParts.HOURS, -step_size)]
+        ).isoformat()
+    )
+    next_from_diti = urllib.parse.quote_plus(
+        diti.diti_op(
+            from_dt, [diti.DitiOpAdd(diti.DitiParts.HOURS, step_size)]
+        ).isoformat()
+    )
+    next_to_diti = urllib.parse.quote_plus(
+        diti.diti_op(
+            to_dt, [diti.DitiOpAdd(diti.DitiParts.HOURS, step_size)]
+        ).isoformat()
+    )
+    zin_from_diti = urllib.parse.quote_plus(
+        diti.diti_op(
+            from_dt, [diti.DitiOpAdd(diti.DitiParts.HOURS, step_size)]
+        ).isoformat()
+    )
+    zin_to_diti = urllib.parse.quote_plus(
+        diti.diti_op(
+            to_dt, [diti.DitiOpAdd(diti.DitiParts.HOURS, -step_size)]
+        ).isoformat()
+    )
+    zout_from_diti = urllib.parse.quote_plus(
+        diti.diti_op(
+            from_dt, [diti.DitiOpAdd(diti.DitiParts.HOURS, -step_size)]
+        ).isoformat()
+    )
+    zout_to_diti = urllib.parse.quote_plus(
+        diti.diti_op(
+            to_dt, [diti.DitiOpAdd(diti.DitiParts.HOURS, step_size)]
+        ).isoformat()
+    )
 
     return render_template(
-        "index.html", dates=json.dumps(dates), data=json.dumps(json_data)
+        "index.html",
+        diti_list=json.dumps(diti_list),
+        data_list=json.dumps(data_list),
+        prev_url_params=f"fDiti={prev_from_diti}&tDiti={prev_to_diti}",
+        next_url_params=f"fDiti={next_from_diti}&tDiti={next_to_diti}",
+        zin_url_params=f"fDiti={zin_from_diti}&tDiti={zin_to_diti}",
+        zout_url_params=f"fDiti={zout_from_diti}&tDiti={zout_to_diti}",
+        to_diti=to_diti,
     )
 
+
 if __name__ == "__main__":
-    app.run("0.0.0.0", 80, debug=(MODE=="debug"))
+    app.run("0.0.0.0", 80, debug=(MODE == "debug"))
---
+++ helper.py
@@ -0,0 +1,85 @@
+import yfinance as yf
+from datetime import datetime
+import diti
+from typing import Tuple
+
+
+def get_initial_filters() -> Tuple[str, str]:
+    now = diti.diti_op(
+        datetime.now(),
+        [
+            diti.DitiOpTimezoneChange(diti.DitiTimezone.EUROPE__ISTANBUL),
+            diti.DitiOpTailOf(diti.DitiParts.DAYS),
+        ],
+    )
+    sevendays_ago = diti.diti_op(
+        now,
+        [
+            diti.DitiOpAdd(diti.DitiParts.DAYS, -7),
+            diti.DitiOpHeadOf(diti.DitiParts.DAYS),
+        ],
+    )
+    return sevendays_ago.isoformat(), now.isoformat()
+
+
+def prepare_index_page_data(
+    from_diti: str, to_diti: str, data_interval: str, data_type: str
+) -> Tuple[list, dict]:
+    data = yf.download(
+        "TRY=X XU100.IS GC=F BTC-USD",
+        start=datetime.fromisoformat(from_diti),
+        end=datetime.fromisoformat(to_diti),
+        interval=data_interval,
+    )
+
+    data = data[
+        [
+            (data_type, "TRY=X"),
+            (data_type, "XU100.IS"),
+            (data_type, "GC=F"),
+            (data_type, "BTC-USD"),
+        ]
+    ]
+
+    if data_interval == "1h":
+        data.index = data.index.floor("H")
+        data = data.groupby(data.index).mean()
+    elif data_interval == "15m":
+        data.index = data.index.floor("15T")
+        data = data.groupby(data.index).mean()
+    data = data.dropna()
+    data[(data_type, "GC=FxTRY=X")] = (
+        data[(data_type, "GC=F")] * data[(data_type, "TRY=X")]
+    )
+    data[(data_type, "BTC-USDxTRY=X")] = (
+        data[(data_type, "BTC-USD")] * data[(data_type, "TRY=X")]
+    )
+
+    data = data[
+        [
+            (data_type, "TRY=X"),
+            (data_type, "XU100.IS"),
+            (data_type, "GC=FxTRY=X"),
+            (data_type, "BTC-USDxTRY=X"),
+        ]
+    ]
+    data = data / data.iloc[0]
+
+    index_mapping = {
+        "TRY=X": {"name": "USD"},
+        "XU100.IS": {"name": "BIST"},
+        "GC=FxTRY=X": {"name": "GOLD"},
+        "BTC-USDxTRY=X": {"name": "BTC"},
+    }
+
+    ditis = [
+        diti.diti_op(
+            e.timestamp(),
+            [diti.DitiOpTimezoneChange(diti.DitiTimezone.EUROPE__ISTANBUL)],
+        ).isoformat()
+        for e in data.index.to_list()
+    ]
+
+    data = {index_mapping[k[1]]["name"]: v for k, v in data.to_dict(orient="list").items()}
+
+    return ditis, data
