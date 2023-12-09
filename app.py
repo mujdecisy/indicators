ac844d32f82509c9d46fec599ca7db9b0934ac7c
mujdecisy <mujdecisy@gmail.com>
2023-12-10 01:51:40 +0300

    feat: add asset detail

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 import json
-from flask import Flask, redirect, render_template, request
+import logging
+from flask import Flask, render_template, request
 from flask_basicauth import BasicAuth
 import os
 import diti
 from datetime import datetime
-import urllib
 
-from helper import get_initial_filters, prepare_chart_actions, prepare_index_page_data
+from helper import get_initial_filters, prepare_chart_actions, prepare_detail_page_data, prepare_index_page_data
 
 app = Flask(__name__)
 MODE = os.environ.get("APP_MODE", "debug")
@@ -18,8 +18,9 @@ app.config["BASIC_AUTH_PASSWORD"] = os.environ.get("APP_BA_PASSWORD", "")
 
 basic_auth = BasicAuth(app)
 
-@app.errorhandler(Exception)
+@app.errorhandler(ValueError)
 def error_handler(err):
+    logging.error(err)
     return render_template("error.html", error_message = str(err))
 
 @app.route("/")
@@ -61,10 +62,59 @@ def index():
         "index.html",
         diti_list=json.dumps(diti_list),
         data_list=json.dumps(data_list),
+        url_path="/",
         prev_url_params=f"fDiti={chart_actions.prev.from_}&tDiti={chart_actions.prev.to}",
         next_url_params=f"fDiti={chart_actions.next.from_}&tDiti={chart_actions.next.to}",
         zin_url_params=f"fDiti={chart_actions.zoomin.from_}&tDiti={chart_actions.zoomin.to}",
         zout_url_params=f"fDiti={chart_actions.zoomout.from_}&tDiti={chart_actions.zoomout.to}",
+        to_diti=to_diti,
+    )
+
+
+@app.route("/assets/<asset_code>")
+@basic_auth.required
+def detail(asset_code: str):
+    init_from, init_to = get_initial_filters()
+
+    data_type = "Close"
+    from_diti = request.args.get("fDiti", init_from)
+    to_diti = request.args.get("tDiti", init_to)
+
+    diff = diti.diti_calcs.diff(
+        datetime.fromisoformat(from_diti),
+        datetime.fromisoformat(to_diti),
+        diti.DitiParts.HOURS,
+    )
+
+    if diff > 8 * 24:
+        data_interval = "1d"
+    elif diff > 2 * 24:
+        data_interval = "1h"
+    else:
+        data_interval = "15m"
+
+    diti_list, data_dict = prepare_detail_page_data(
+        asset_code, from_diti, to_diti, data_interval, data_type
+    )
+
+    data_list = [{"name": k, "data": v} for k, v in data_dict.items()]
+
+    from_dt = datetime.fromisoformat(from_diti)
+    to_dt = datetime.fromisoformat(to_diti)
+
+    step_size = int(diff / 4)
+
+    chart_actions = prepare_chart_actions(from_dt, to_dt, step_size)
+
+    return render_template(
+        "asset.html",
+        diti_list=json.dumps(diti_list),
+        data_list=json.dumps(data_list),
+        url_path=f"/assets/{asset_code}",
+        prev_url_params=f"aCode={asset_code}&fDiti={chart_actions.prev.from_}&tDiti={chart_actions.prev.to}",
+        next_url_params=f"aCode={asset_code}&fDiti={chart_actions.next.from_}&tDiti={chart_actions.next.to}",
+        zin_url_params=f"aCode={asset_code}&fDiti={chart_actions.zoomin.from_}&tDiti={chart_actions.zoomin.to}",
+        zout_url_params=f"aCode={asset_code}&fDiti={chart_actions.zoomout.from_}&tDiti={chart_actions.zoomout.to}",
         to_diti=to_diti,
     )
 
--- helper.py
+++ helper.py
@@ -4,6 +4,7 @@ import diti
 from typing import Tuple
 import urllib
 
+
 class ActionDiti:
     from_: str
     to: str
@@ -12,11 +13,21 @@ class ActionDiti:
         self.from_ = from_
         self.to = to
 
+
 class ChartActions:
     prev: ActionDiti
     next_: ActionDiti
     zoomin: ActionDiti
     zoomout: ActionDiti
+
+
+index_mapping = {
+    "TRY=X": {"name": "USD"},
+    "XU100.IS": {"name": "BIST"},
+    "GC=FxTRY=X": {"name": "GOLD"},
+    "BTC-USDxTRY=X": {"name": "BTC"},
+}
+
 
 def get_initial_filters() -> Tuple[str, str]:
     now = diti.diti_op(
@@ -58,15 +69,22 @@ def prepare_index_page_data(
     ]
 
     if data_interval == "1h":
+        try:
+            getattr(data.index, "floor")
+        except AttributeError:
+            raise ValueError("No data found for")
         data.index = data.index.floor("H")
         data = data.groupby(data.index).mean()
-    elif data_interval == "15T":
+    elif data_interval == "15m":
+        try:
+            getattr(data.index, "floor")
+        except AttributeError:
+            raise ValueError("No data found for")
         data.index = data.index.floor("15T")
         data = data.groupby(data.index).mean()
     data = data.dropna()
     if len(data) < 1:
         raise ValueError(f"No data found between [{from_diti}] - [{to_diti}]")
-
 
     data[(data_type, "GC=FxTRY=X")] = (
         data[(data_type, "GC=F")] * data[(data_type, "TRY=X")]
@@ -85,13 +103,6 @@ def prepare_index_page_data(
     ]
     data = data / data.iloc[0]
 
-    index_mapping = {
-        "TRY=X": {"name": "USD"},
-        "XU100.IS": {"name": "BIST"},
-        "GC=FxTRY=X": {"name": "GOLD"},
-        "BTC-USDxTRY=X": {"name": "BTC"},
-    }
-
     ditis = [
         diti.diti_op(
             e.timestamp(),
@@ -100,12 +111,16 @@ def prepare_index_page_data(
         for e in data.index.to_list()
     ]
 
-    data = {index_mapping[k[1]]["name"]: v for k, v in data.to_dict(orient="list").items()}
+    data = {
+        index_mapping[k[1]]["name"]: v for k, v in data.to_dict(orient="list").items()
+    }
 
     return ditis, data
 
 
-def prepare_chart_actions(from_dt: datetime, to_dt: datetime, step_size: int) -> ChartActions:
+def prepare_chart_actions(
+    from_dt: datetime, to_dt: datetime, step_size: int
+) -> ChartActions:
     prev_from_diti = urllib.parse.quote_plus(
         diti.diti_op(
             from_dt, [diti.DitiOpAdd(diti.DitiParts.HOURS, -step_size)]
@@ -154,3 +169,72 @@ def prepare_chart_actions(from_dt: datetime, to_dt: datetime, step_size: int) ->
     chart_actions.zoomout = ActionDiti(zout_from_diti, zout_to_diti)
 
     return chart_actions
+
+
+def prepare_detail_page_data(
+    asset_code: str, from_diti: str, to_diti: str, data_interval: str, data_type: str
+) -> Tuple[list, dict]:
+    assets_to_download = f"{asset_code} {'TRY=X' if asset_code in ['GC=F', 'BTC-USD'] else ''}"
+
+    data = yf.download(
+        assets_to_download,
+        start=datetime.fromisoformat(from_diti),
+        end=datetime.fromisoformat(to_diti),
+        interval=data_interval,
+    )
+    if len(data) < 1:
+        raise ValueError(f"No data found between [{from_diti}] - [{to_diti}]")
+
+    if asset_code in ['GC=F', 'BTC-USD']:
+        data = data[[(data_type, "TRY=X"), (data_type, asset_code)]]
+    else:
+        data = data[[(data_type)]]
+
+    if data_interval == "1h":
+        data.index = data.index.floor("H")
+        data = data.groupby(data.index).mean()
+    elif data_interval == "15m":
+        data.index = data.index.floor("15T")
+        data = data.groupby(data.index).mean()
+    data = data.dropna()
+
+    if len(data) < 1:
+        raise ValueError(f"No data found between [{from_diti}] - [{to_diti}]")
+
+    if asset_code == "GC=F":
+        data[(data_type, "GC=FxTRY=X")] = (
+            data[(data_type, "GC=F")] * data[(data_type, "TRY=X")]
+        )
+        asset_code = "GC=FxTRY=X"
+    elif asset_code == "BTC-USD":
+        data[(data_type, "BTC-USDxTRY=X")] = (
+            data[(data_type, "BTC-USD")] * data[(data_type, "TRY=X")]
+        )
+        asset_code = "BTC-USDxTRY=X"
+
+    if asset_code in ['GC=F', 'BTC-USD']:
+        data = data[
+            [
+                (data_type, asset_code),
+            ]
+        ]
+    else:
+        data = data[
+            [
+                (data_type),
+            ]
+        ]
+
+    ditis = [
+        diti.diti_op(
+            e.timestamp(),
+            [diti.DitiOpTimezoneChange(diti.DitiTimezone.EUROPE__ISTANBUL)],
+        ).isoformat()
+        for e in data.index.to_list()
+    ]
+
+    data = {
+        index_mapping[asset_code]["name"]: v for k, v in data.to_dict(orient="list").items()
+    }
+
+    return ditis, data
